100e63bba29ed4a5e35fa0522de61f4f26471079
NXK <neo.kirbis@gmail.com>
2025-06-08 16:12:27 +0200

    Stop dev console from opening on startup

--- app/src/main/index.ts
+++ app/src/main/index.ts
@@ -39,7 +39,6 @@ function createWindow(): void {
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
-    mainWindow.webContents.openDevTools()
   })
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
